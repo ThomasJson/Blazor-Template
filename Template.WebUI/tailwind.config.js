5b2e453323a6455bafa6f91c4c659e66ea5e9fd7
ThomasJson <joogletom8@gmail.com>
2024-07-19 16:18:10 +0200

    Add TailwindCSS to Blazor Template

---
+++ Template.WebUI/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: [
+        "./**/*.razor",
+        "./**/*.cshtml"
+    ],
+
+    theme: {
+        extend: {
+            spacing: {
+                '13%': '13%',
+                '87%': '87%'
+            }
+        },
+    },
+
+    plugins: [],
+}
